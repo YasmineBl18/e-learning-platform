b9a6b9fdf6ac461e235612898592e99941acce8a
Yasmine Bourijal <yasbourijal@gmail.com>
2025-11-20 23:14:28 +0100

    Discovery-service et Gateway-service

--- course-service/src/main/java/org/example/courseservice/repositories/CourseRepository.java
+++ course-service/src/main/java/org/example/courseservice/repositories/CourseRepository.java
@@ -2,9 +2,7 @@ package org.example.courseservice.repositories;
 
 import org.example.courseservice.entities.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
--- enrollment-service/src/main/java/org/example/enrollmentservice/repositories/EnrollmentRepository.java
+++ enrollment-service/src/main/java/org/example/enrollmentservice/repositories/EnrollmentRepository.java
@@ -2,8 +2,6 @@ package org.example.enrollmentservice.repositories;
 
 import org.example.enrollmentservice.entities.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 }
